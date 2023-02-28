5fb8714f939f5b2ced3d6fd98026e4429283c81c
DevMattyJS <matej.sliacky@protonmail.com>
2023-02-28 20:22:07 +0100

    Basic React Navigation setup added

---
+++ screens/ManageExpense.js
@@ -0,0 +1,9 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function ManageExpense() {
+  return <Text>Manage Expense Screen</Text>;
+}
+
+export default ManageExpense;
+
+const styles = StyleSheet.create({});
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,9 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function RecentExpenses() {
+  return <Text>Recent Expenses Screen</Text>;
+}
+
+export default RecentExpenses;
+
+const styles = StyleSheet.create({});
