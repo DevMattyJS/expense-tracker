64dbf65693b7b6939e15d027abea5f3739beffd1
DevMattyJS <matej.sliacky@protonmail.com>
2023-03-11 17:36:48 +0100

    Re-factored to use ids from firebase instead on our own

--- util/http.js
+++ util/http.js
@@ -3,15 +3,20 @@ import axios from "axios"; // a 3rd party package to make http request simpler
 const BACKEND_URL =
   "https://expense-tracker-mobile-27d44-default-rtdb.europe-west1.firebasedatabase.app/";
 
-export function storeExpense(expenseData) {
-  axios.post(BACKEND_URL + "/expenses.json", expenseData);
+export async function storeExpense(expenseData) {
+  const response = await axios.post(
+    BACKEND_URL + "/expenses.json",
+    expenseData
+  );
+  const id = response.data.name; // accessing the auto-generated id from firebase database
+  return id;
 }
 
 export async function fetchExpenses() {
   const response = await axios.get(BACKEND_URL + "/expenses.json");
 
   const expenses = [];
-  console.log(response.data);
+
   for (const key in response.data) {
     const expenseObj = {
       id: key,
